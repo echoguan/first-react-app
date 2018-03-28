7b3e65a5b5f333dfcd8bb8a8823c63f108816997
echo.guan <echo.guan@perficient.com>
2018-03-28 14:55:06 +0800

    want to fix antd-mobile

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Logo from "../../component/logo/logo";
-import { WingBlank, WhiteSpace, Button } from "antd-mobile";
+// import { WingBlank, WhiteSpace, Button } from "antd-mobile";
 // import "antd-mobile/dist/antd-mobile.css";
+import { Button } from 'antd-mobile';
+// import 'antd-mobile/dist/antd-mobile.css';
 
 class Register extends React.Component {
   render() {
@@ -9,11 +11,11 @@ class Register extends React.Component {
       <div>
         <Logo />
         <h2>Register Page</h2>
-        <WingBlank>
+        {/* <WingBlank> */}
           <Button type="primary">登录</Button>
-          <WhiteSpace />
-          <Button type="primary">注册</Button>
-        </WingBlank>
+          {/* <WhiteSpace /> */}
+          {/* <Button type="primary">注册</Button> */}
+        {/* </WingBlank> */}
       </div>
     );
   }
