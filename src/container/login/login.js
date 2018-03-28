9069f6444b436a989cc7263e01f9451878c594f9
echo.guan <echo.guan@perficient.com>
2018-03-28 17:11:23 +0800

    custom config; modify login and register page

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,12 +1,35 @@
 import React from "react";
 import Logo from "../../component/logo/logo";
+import { WingBlank, WhiteSpace, Button, List, InputItem } from "antd-mobile";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.register = this.register.bind(this);
+  }
+
+  register() {
+    this.props.history.push("./register");
+  }
+
   render() {
     return (
       <div>
         <Logo />
-        <h2>我是登录页面</h2>
+        <h2>请登录</h2>
+        <WingBlank>
+          <List>
+            <InputItem>用户</InputItem>
+            <InputItem>密码</InputItem>
+          </List>
+          <WhiteSpace />
+          <Button type="primary">登录</Button>
+          <WhiteSpace />
+          <Button onClick={this.register} type="primary">
+            注册
+          </Button>
+        </WingBlank>
       </div>
     );
   }
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,21 +1,49 @@
 import React from "react";
 import Logo from "../../component/logo/logo";
-// import { WingBlank, WhiteSpace, Button } from "antd-mobile";
-// import "antd-mobile/dist/antd-mobile.css";
-import { Button } from 'antd-mobile';
-// import 'antd-mobile/dist/antd-mobile.css';
+import {
+  WingBlank,
+  WhiteSpace,
+  Button,
+  List,
+  InputItem,
+  Radio
+} from "antd-mobile";
+
+const RadioItem = Radio.RadioItem;
+
+function onChange(e) {
+  console.log(`radio checked:${e.target.value}`);
+}
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "genius"
+    };
+  }
+
   render() {
     return (
       <div>
         <Logo />
-        <h2>Register Page</h2>
-        {/* <WingBlank> */}
-          <Button type="primary">登录</Button>
-          {/* <WhiteSpace /> */}
-          {/* <Button type="primary">注册</Button> */}
-        {/* </WingBlank> */}
+        <h2>用户注册</h2>
+        <WingBlank>
+          <List>
+            <InputItem>用户名</InputItem>
+            <InputItem>密码</InputItem>
+            <InputItem>确认密码</InputItem>
+          </List>
+
+          <WhiteSpace />
+          <List>
+            <RadioItem checked={this.state.type === "genius"}>牛人</RadioItem>
+            <RadioItem checked={this.state.type === "boss"}>BOSS</RadioItem>
+          </List>
+
+          <WhiteSpace />
+          <Button>注册</Button>
+        </WingBlank>
       </div>
     );
   }
