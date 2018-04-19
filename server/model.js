3c8294c09823577f647d6ea51b83e0a1a7e055bd
echo.guan <echo.guan@perficient.com>
2018-04-19 16:59:39 +0800

    add mongo in progress

--- server/model.js
+++ server/model.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
 
-const DB_URL = "mongodb://localhost:27017/boss";
+const DB_URL = "mongodb://localhost:27017/boss-chat";
 mongoose.connect(DB_URL);
+
+const models = {
+  user: {
+    user: { type: String, require: true },
+    pwd: { type: String, require: true },
+    type: { type: String, require: true },
+    avatar: { type: String },
+    desc: { type: String },
+    title: { type: String },
+    company: { type: String },
+    money: { type: String }
+  },
+  chat: {}
+};
