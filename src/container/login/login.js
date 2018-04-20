9cfa545e6788c7fe564dc2116c78159b92d0de7b
echo.guan <echo.guan@perficient.com>
2018-04-20 16:59:20 +0800

    增加login, in progress

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Logo from "../../component/logo/logo";
 import { WingBlank, WhiteSpace, Button, List, InputItem } from "antd-mobile";
+import { connect } from "react-redux";
+import { login } from "../../redux/user.redux";
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: "",
+      password: ""
+    };
 
     this.register = this.register.bind(this);
   }
 
   register() {
     this.props.history.push("./register");
+  }
+
+  handleChange(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  handleLogin() {
+    this.props.login(this.state);
   }
 
   render() {
@@ -20,11 +34,20 @@ class Login extends React.Component {
         <h2>请登录</h2>
         <WingBlank>
           <List>
-            <InputItem>用户</InputItem>
-            <InputItem>密码</InputItem>
+            <InputItem onChange={v => this.handleChange("username", v)}>
+              用户
+            </InputItem>
+            <InputItem
+              type="password"
+              onChange={v => this.handleChange("password", v)}
+            >
+              密码
+            </InputItem>
           </List>
           <WhiteSpace />
-          <Button type="primary">登录</Button>
+          <Button type="primary" onClick={this.handleLogin}>
+            登录
+          </Button>
           <WhiteSpace />
           <Button onClick={this.register} type="primary">
             注册
@@ -35,4 +58,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default connect(state => state.username, { login })(Login);
