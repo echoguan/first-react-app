53ae17ac8bcad309f0c98c991b75b7884369ed3d
echo.guan <echo.guan@perficient.com>
2018-04-25 17:03:40 +0800

    增加注册密码MD5加密

--- server/user.js
+++ server/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router();
 const models = require("./model");
 const User = models.getModel("user");
+const utils = require("utility");
 
 Router.get("/list", function(req, res) {
   User.find({}, function(err, doc) {
@@ -15,7 +16,10 @@ Router.post("/register", function(req, res) {
     if (doc) {
       return res.json({ code: 1, msg: "用户名已存在..." });
     }
-    User.create({ username, password, type }, function(err, doc) {
+    User.create({ username, type, password: md5Pwd(password) }, function(
+      err,
+      doc
+    ) {
       if (err) {
         return res.json({ code: 1, msg: "服务器出错，请稍后再试..." });
       }
@@ -27,5 +31,11 @@ Router.post("/register", function(req, res) {
 Router.get("/info", function(req, res) {
   return res.json({ code: 1 });
 });
+
+function md5Pwd(password) {
+  const salt1 = "disk@adj123#sdfkja@gxq";
+  const salt2 = "mask!sick0;sdfkja:zxc";
+  return utils.md5(utils.md5(salt1 + password + salt2));
+}
 
 module.exports = Router;
