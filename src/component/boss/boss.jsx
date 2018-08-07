16265ee088729efc7844ad932b04da1ec678733e
echoguan <echo_guan@outlook.com>
2018-08-07 23:34:21 +0800

    牛人列表完成

--- src/component/boss/boss.jsx
+++ src/component/boss/boss.jsx
@@ -26,12 +26,17 @@ class Boss extends React.Component {
                 {this.state.data.map(
                     v =>
                         v.avatar ? (
-                            <Card>
+                            <Card key={v._id}>
                                 <Card.Header
                                     title={v.user}
                                     thumb={require(`../img/${v.avatar}.png`)}
                                     extra={<span>{v.title}</span>}
                                 />
+                                <Card.Body>
+                                    {v.desc
+                                        .split("\n")
+                                        .map(v => <div key={v}>{v}</div>)}
+                                </Card.Body>
                             </Card>
                         ) : null
                 )}
